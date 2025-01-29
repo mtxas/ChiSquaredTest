c5467373a0892fd26ab1f7be9c9b961940cab1d5
mtxas <maaatasinfo@gmail.com>
2025-01-29 22:57:52 +0100

    Some other things useful for packages.

--- R/sample_document.R
+++ R/sample_document.R
@@ -1,8 +1,8 @@
 #' Sample Function
 #'
-#' @param x
+#' @param x An integer
 #'
-#' @returns
+#' @returns An almost random number.
 #' @export
 #'
 #' @examples
@@ -23,4 +23,17 @@ sample_function <- function(x = 5){
 # Nachdem wir jetzt irgendwas gemacht haben (ich glaube, DESCRIPTION geandert,
 # hat sich auch README.md geandert)
 # bitte nutzen use_readme_rmd(), damit auch eine README.RMD Datei erstellt wird, nicht nur README.md
-#
+# git add .
+# git status
+# git commit -m "my message"
+# git push
+# Vllt moechte man Daten speichern:
+# x <- sample(1:100, 50, replace = TRUE)
+# library(devtools)
+# use_data(x)
+# Es wird ein Folder data erstellt und eine Datei x.rda
+# Diese kann dann jemand spaeter benutzen.
+# Man kann auch machen:
+# df <- data.frame(x)
+# use_data(df)
+# (es wird df.rda erstellt)
