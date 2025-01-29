e9dc311f82f5814dc7497677f959ae83b3015788
mtxas <maaatasinfo@gmail.com>
2025-01-29 22:44:20 +0100

    New package.

---
+++ R/sample_document.R
@@ -0,0 +1,26 @@
+#' Sample Function
+#'
+#' @param x
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+#' sample_function()
+#' sample_function(3)
+sample_function <- function(x = 5){
+  return(x+1)
+}
+
+# getwd()
+# Env loeschen
+# load_all()
+# Alt + Ctrl + Shift + R: Insert Oxygen Skeleton (or Code > Insert Oxygen Skeleton)
+# document()
+# @export macht es visible fuer den Nutzer. Wenn man die Funktion
+# nicht fuer den Nutzer geben moechte, dann @export wegnehmen.
+# Dokumentation Files sind in /man
+# Nachdem wir jetzt irgendwas gemacht haben (ich glaube, DESCRIPTION geandert,
+# hat sich auch README.md geandert)
+# bitte nutzen use_readme_rmd(), damit auch eine README.RMD Datei erstellt wird, nicht nur README.md
+#
